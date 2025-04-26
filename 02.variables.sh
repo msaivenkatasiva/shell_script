ac95f9a217ac3c314493a84ce2e58d7042d7dac9
msaivenkatasiva <sivasl0597@gmail.com>
2025-04-27 01:01:18 +0530

    variables

---
+++ 02.variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "baleno: Hi, I'm baleno"
+echo "nexon: Hi, I'm nexon :)"
+echo "baleno: i have the most exiting features, and i'm the most comfortable vehicle"
+echo "nexon: i'm the most safest car"
+echo "baleno left the chat"
