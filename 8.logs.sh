fc52b26eec73ce69c4fd75a13bbd2f151dd426c9
msaivenkatasiva <sivasl0597@gmail.com>
2025-04-27 15:26:01 +0530

    condition

--- 8.logs.sh
+++ 8.logs.sh
@@ -2,13 +2,18 @@
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then    
         echo "error::....installing $2 failed"
         exit 1
     else
-        echo " $2 success"
+        echo "$G $2 success"
     fi
 }
 
@@ -18,7 +23,7 @@ then
     echo "please run the command with root user"
     exit 1
 else    
-    echo "you are root user"
+    echo "$Y you are root user"
 fi
 
 yum install git -y &>> $LOGFILE
