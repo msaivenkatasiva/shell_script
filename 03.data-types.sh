480b3620a18947ccd23215fe62bf61e0864d82b3
msaivenkatasiva <sivasl0597@gmail.com>
2025-04-27 08:12:47 +0530

    first variable trail

--- 03.data-types.sh
+++ 03.data-types.sh
@@ -2,6 +2,6 @@ NUMBER1=$1
 NUMBER2=$2
 DATE=$(date)
 
-echo"the sum executed at $DATE"
+echo "the sum executed at $DATE"
 $SUM=(($NUMBER1=$NUMBER2))
-echo"total amount is $SUM"
+echo "total amount is $SUM"  
