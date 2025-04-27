273d1d8574539fb607989fcd8cc8751f3dd2fcf9
msaivenkatasiva <sivasl0597@gmail.com>
2025-04-27 10:09:39 +0530

    condition

--- 05.conditions.sh
+++ 05.conditions.sh
@@ -3,10 +3,10 @@
 NUMBER=$1
 DATE=$(date)
 
-echo "(the statement is delivered at $DATE)"
+echo "the statement is delivered at $DATE"
 
 if [ $NUMBER is -gt 100 ]
-    echo "(given $NUMBER is greater than 100)"
+    echo "given $NUMBER is greater than 100"
 else
-    echo "(given $NUMBER is less than 100)"
+    echo "given $NUMBER is less than 100"
 fi
