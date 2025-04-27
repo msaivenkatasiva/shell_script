e20c5e7f31ae0d30d621540abf34d154f718ea39
msaivenkatasiva <sivasl0597@gmail.com>
2025-04-27 11:07:03 +0530

    condition

---
+++ 06.installingmysql.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+ID=$(id)
+
+if [ $ID -ne 0 ]
+then
+    echo "please run the command with root user"
+    EXIT 1
+else
+    echo "you are youser"
+fi
+
+yum install mysql -y
+
+if [$? -ne 0]
+then 
+    echo "error:: installing my sql failed"
+    EXIT 1
+else
+    echo "installing git is sucessfull"
+fi
