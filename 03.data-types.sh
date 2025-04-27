add01583a39ac5ff507ee5cbee1a68c31d4eada7
msaivenkatasiva <sivasl0597@gmail.com>
2025-04-27 18:03:39 +0530

    condition

--- 03.data-types.sh
+++ 03.data-types.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 NUMBER1=$1
 NUMBER2=$2
 DATE=$(date)
 
-echo "the sum executed at $DATE"
+echo "the sum executed at $G $DATE $N"
 SUM=$(($NUMBER1+$NUMBER2))
-echo "total amount is $SUM"  
+echo "total amount is $R $SUM $N"  
