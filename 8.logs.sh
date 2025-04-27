bae104aa0a1416a1746e198d4d9c6bfb29d1dbb8
msaivenkatasiva <sivasl0597@gmail.com>
2025-04-27 15:04:46 +0530

    condition

---
+++ 8.logs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then    
+        echo "error::....installing $2 failed"
+        exit 1
+    else
+        echo " $2 success"
+    fi
+}
+
+ID=$(id)
+if [ $ID -ne 0 ]
+then 
+    echo "please run the command with root user"
+    exit 1
+else    
+    echo "you are root user"
+fi
+
+yum install git -y
+
+VALIDATE $? "installing git"
