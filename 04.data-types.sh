713a1b0cba635d4c2a2f06587ed500cac69f3fbf
msaivenkatasiva <sivasl0597@gmail.com>
2025-04-27 08:27:58 +0530

    datatypes

---
+++ 04.data-types.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+FRUITS=("apple" "banana" "mango")
+
+echo "first value: ${FRUITS[1]}"
+echo "second value: ${FRUITS[2]}"
+echo "third value: ${FRUITS[3]}"
+echo "all fruits: ${FRUITS[@]}"
