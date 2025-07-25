48765af3d89d73393b48f9a6d1a11570c43c1a2f
msaivenkatasiva <sivasl0597@gmail.com>
2025-07-25 09:19:32 +0530

    call other script

---
+++ 15.current-script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+COURSE="DevOps from current script"
+
+echo "before calling other script, course is $COURSE"
+echo "process id of current shell script: $$"
+
+#./16.other-script.sh
+
+echo "after calling other script $COURSE"
