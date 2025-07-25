9f482e4b4eb46c8eab5820700ddb54e1a19a030f
msaivenkatasiva <sivasl0597@gmail.com>
2025-07-25 08:04:01 +0530

    call other script

--- 01.current-script.sh
+++ 01.current-script.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+
 COURSE="devops from current script"
 
 echo "before calling other script my course is $COURSE"
 echo "my process instance ID is $$"
 
-source ./02.other-script.sh
+./02.other-script.sh
 
 echo "my course is $COURSE"
 echo "my process instance id is $$"
