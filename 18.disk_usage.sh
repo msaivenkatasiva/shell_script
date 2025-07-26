4bf2c53146c1537f408ef5bb185f6673fd6f2bdc
msaivenkatasiva <sivasl0597@gmail.com>
2025-07-26 22:44:22 +0530

    triggering disk usage

--- 18.disk_usage.sh
+++ 18.disk_usage.sh
@@ -8,7 +8,7 @@ while IFS= read -r line
 do
     USAGE=$( echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1 )
     FOLDER=$( echo $line | df -hT | grep xfs | awk -F " " '{prinf NF}')
-    if [ $DISK_USAGE -ge $DISK_THRESHOLD ]
+    if [ $USAGE -ge $DISK_THRESHOLD ]
     then
         echo "$FOLDER is more than $DISK_THRESHOLD, current usage is $USAGE"
     fi
